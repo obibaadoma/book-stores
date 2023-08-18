82d7fe6672470fb82964266a663cb18e7ffb57c0
obibaadoma <akrasikonadu@yahoo.com>
2023-08-18 18:09:41 +0000

    setup and install

---
+++ my-book-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchBooks } from './components/redux/books/books';
+import BookList from './components/BookList';
+import Navigation from './components/NavBar';
+import CategoryPage from './components/Category';
+
+const App = () => {
+  const dispatch = useDispatch();
+  dispatch(fetchBooks());
+  return (
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/category" element={<CategoryPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
